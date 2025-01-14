a7df49671e83ec74d6659921f98a35c7c624a8b2
Albina Januzi <albina_januzi@hotmail.com>
2025-01-14 16:23:46 +0100

    height in meter & weight in kg

--- scripts/template.js
+++ scripts/template.js
@@ -27,14 +27,31 @@ async function showDetails(pokemonId) {
     for (let i = 0; i < pokemon.types.length; i++) {
       typesHTML += `<span class="type ${pokemon.types[i].type.name}">${pokemon.types[i].type.name}</span>`;
     }
-  
+
+    let abilitiesHTML = '';
+    if (pokemon.abilities && pokemon.abilities.length > 0) {
+        for (let i = 0; i < pokemon.abilities.length; i++) {
+            abilitiesHTML += `<span class="ability">${pokemon.abilities[i].ability.name}</span>`;
+            if (i < pokemon.abilities.length - 1) {
+                abilitiesHTML += ', ';
+            }
+        }
+    } else {
+        abilitiesHTML = '<span class="ability unknown">No Abilities</span>';
+    }
+    
+    const heightInMeter = (pokemon.height / 10).toFixed(1);
+    const weightInKg = (pokemon.weight / 10).toFixed(1);
+
     details.innerHTML = `
       <h2>${pokemon.name}</h2>
       <img src="${pokemon.sprites.other['official-artwork'].front_default}" alt="${pokemon.name}">
-      <p>Height: ${pokemon.height}</p>
-      <p>Weight: ${pokemon.weight}</p>
+      <p>Height: ${heightInMeter} m</p>
+      <p>Weight: ${weightInKg} kg</p>
       <p>Base Experience: ${pokemon.base_experience}</p>
+      <div class="abilities">Abilities: ${abilitiesHTML}</div>  
       <div class="types">${typesHTML}</div>
+      
     `;
 
     overlay.dataset.currentPokemonId = pokemonId;
